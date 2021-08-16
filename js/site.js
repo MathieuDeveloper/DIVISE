065df225b2e39141d18dd6b653bced92d306aca3
MathieuDeveloper <topfermathieu@yahoo.fr>
2021-08-16 22:41:51 +0200

    fzz buzz instead of dit vise

--- js/site.js
+++ js/site.js
@@ -1,32 +1,32 @@
 // get values, call functions
 function getValues() {
-    let ditValue = document.getElementById("ditValue").value;
-    let viseValue = document.getElementById("viseValue").value;
+    let fizzValue = document.getElementById("fizzValue").value;
+    let buzzValue = document.getElementById("buzzValue").value;
 
-    ditValue = parseInt(ditValue);
-    viseValue = parseInt(viseValue);
+    fizzValue = parseInt(fizzValue);
+    buzzValue = parseInt(buzzValue);
 
 
     
-    displayNumbers(ditValue, viseValue);
+    displayNumbers(fizzValue, buzzValue);
 
 }
 
 
 //view function
-function displayNumbers(ditValue, viseValue) {
+function displayNumbers(fizzValue, buzzValue) {
     let templateTab = ""
 
     for (let i = 1; i <= 100; i++) {
         
-        if (i % ditValue == 0 && i % viseValue == 0) {
+        if (i % fizzValue == 0 && i % buzzValue == 0) {
             templateTab += `<tr><td>FizzBuzz</td></tr>`;
             
         } else {
-            if (i % ditValue == 0 && i % viseValue != 0) {
+            if (i % fizzValue == 0 && i % buzzValue != 0) {
                 templateTab += `<tr><td>Fizz</td></tr>`;               
             } else {
-                if (i % ditValue != 0 && i % viseValue == 0) {
+                if (i % fizzValue != 0 && i % buzzValue == 0) {
                     templateTab += `<tr><td>Buzz</td></tr>`;
                 } else {
                     templateTab += `<tr><td>${i}</td></tr>`;
